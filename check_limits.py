3555fc2aecc36d41aa29d3439733aff919091633
Ritunjay-11 <66157748+Ritunjay-11@users.noreply.github.com>
2023-07-20 16:01:15 +0530

    Update check_limits.py
    split into 3 seperate functions, each with a minimal ccm, duplication still a problem

--- check_limits.py
+++ check_limits.py
@@ -1,18 +1,35 @@
+def is_temperature_out_of_range(temperature):
+    return temperature < 0 or temperature > 45
+
+
+def is_soc_out_of_range(soc):
+    return soc < 20 or soc > 80
+
+
+def is_charge_rate_out_of_range(charge_rate):
+    return charge_rate > 0.8
+
 
 def battery_is_ok(temperature, soc, charge_rate):
-  if temperature < 0 or temperature > 45:
-    print('Temperature is out of range!')
-    return False
-  elif soc < 20 or soc > 80:
-    print('State of Charge is out of range!')
-    return False
-  elif charge_rate > 0.8:
-    print('Charge rate is out of range!')
-    return False
+    if is_temperature_out_of_range(temperature):
+        print('Temperature is out of range!')
+        return False
+    elif is_soc_out_of_range(soc):
+        print('State of Charge is out of range!')
+        return False
+    elif is_charge_rate_out_of_range(charge_rate):
+        print('Charge rate is out of range!')
+        return False
 
-  return True
+    return True
 
 
 if __name__ == '__main__':
-  assert(battery_is_ok(25, 70, 0.7) is True)
-  assert(battery_is_ok(50, 85, 0) is False)
+    # Positive test cases
+    assert battery_is_ok(25, 70, 0.7) is True
+    assert battery_is_ok(0, 20, 0.8) is True
+
+    # Negative test cases
+    assert battery_is_ok(50, 85, 0) is False
+    assert battery_is_ok(-5, 60, 0.5) is False
+    assert battery_is_ok(30, 10, 0.9) is False
