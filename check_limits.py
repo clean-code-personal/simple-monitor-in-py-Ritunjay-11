75318570508a33588f9748a5b2c5a4523ff7dcce
Ritunjay-11 <66157748+Ritunjay-11@users.noreply.github.com>
2023-07-20 16:03:02 +0530

    Update check_limits.py
    solves duplication, further tests based on articles need to be incorporated

--- check_limits.py
+++ check_limits.py
@@ -1,35 +1,23 @@
-def is_temperature_out_of_range(temperature):
-    return temperature < 0 or temperature > 45
-
-
-def is_soc_out_of_range(soc):
-    return soc < 20 or soc > 80
-
-
-def is_charge_rate_out_of_range(charge_rate):
-    return charge_rate > 0.8
+def out_of_range(value, min_val, max_val, parameter_name):
+    if value < min_val or value > max_val:
+        print(f'{parameter_name} is out of range!')
+        return True
+    return False
 
 
 def battery_is_ok(temperature, soc, charge_rate):
-    if is_temperature_out_of_range(temperature):
-        print('Temperature is out of range!')
-        return False
-    elif is_soc_out_of_range(soc):
-        print('State of Charge is out of range!')
-        return False
-    elif is_charge_rate_out_of_range(charge_rate):
-        print('Charge rate is out of range!')
+    if is_parameter_out_of_range(temperature, 0, 45, 'Temperature') \
+            or is_parameter_out_of_range(soc, 20, 80, 'State of Charge') \
+            or is_parameter_out_of_range(charge_rate, 0, 0.8, 'Charge rate'):
         return False
 
     return True
 
 
 if __name__ == '__main__':
-    # Positive test cases
+    # Test cases
     assert battery_is_ok(25, 70, 0.7) is True
     assert battery_is_ok(0, 20, 0.8) is True
-
-    # Negative test cases
     assert battery_is_ok(50, 85, 0) is False
     assert battery_is_ok(-5, 60, 0.5) is False
     assert battery_is_ok(30, 10, 0.9) is False
